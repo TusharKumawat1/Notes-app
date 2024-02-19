08d055ad3b9bbbfd00b46730f4d87a5554463da3
tushar <tusharkumawat9694@gmail.com>
2024-02-19 15:37:56 +0530

    working

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 const mongoose =require('mongoose');
 const mongoUri='mongodb+srv://tusharkumawat9694:tushar%40@cluster0.bmp19bn.mongodb.net/';
 const connectToMongo=()=>{
-    mongoose.connect(mongoUri ).then (()=>console.log('successfully connected'))
+    mongoose.connect(mongoUri ,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+      }).then (()=>console.log('successfully connected'))
     .catch((err)=>console.log("unable to connect "+err)) ;
 }
 module.exports=connectToMongo; 
